05ff321fd4203cd21e50bbf940d28f709d983474
Ausqa21 <ausqa95@gmail.com>
2017-02-06 20:51:34 +0000

    New relative path to template file.
    The template file was at an absolute location so had to change it to a
    relative location for the server to find it.

--- ublog/views.py
+++ ublog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    return render(request, 'C:/Users/Ausqa21/Desktop/Django/blog_ausqa_p1/ublog/templates/ublog/post_list.html', {})
+    return render(request, './ublog/post_list.html', {})
